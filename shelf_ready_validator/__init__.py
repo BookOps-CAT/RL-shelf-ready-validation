4e984a6d849194009981bee4dad49dbc5307b768
Charlotte Kostelic <charlottekostelic@gmail.com>
2024-01-30 14:48:25 -0500

    edited commands, added help messages

--- shelf_ready_validator/__init__.py
+++ shelf_ready_validator/__init__.py
@@ -2,7 +2,7 @@ import click
 from pydantic import ValidationError
 from rich.console import Console
 from rich.theme import Theme
-
+from functools import update_wrapper
 from shelf_ready_validator.models import MonographRecord, OtherMaterialRecord
 from shelf_ready_validator.errors import format_error_messages
 from shelf_ready_validator.translate import (
@@ -21,73 +21,67 @@ theme = Theme(
 console = Console(tab_size=5, theme=theme)
 
 
-@click.group()
-def cli():
+@click.group(chain=True)
+@click.option(
+    "--file",
+    "file",
+    prompt=True,
+    help="Provide the filename for the file you would like to open.",
+)
+def cli(file):
     """
-    A program that reads and validates MARC records from vendors
+    Read and validate MARC records
+
     """
     pass
 
 
-@cli.command("read-marc")
-@click.option(
-    "-f",
-    "--file",
-    prompt="File",
-    help="Path to file you would like to read or validate.",
-)
-def read_marc(file):
+@cli.result_callback()
+def process_commands(processors, file):
+    # Start with MARC file.
+    reader = read_marc_records(file)
+    # Pipe it through all processors.
+    for processor in processors:
+        reader = processor(reader)
+    for _ in reader:
+        pass
+
+
+def processor(f):
+    def new_func(*args, **kwargs):
+        def processor(stream):
+            return f(stream, *args, **kwargs)
+
+        return processor
+
+    return update_wrapper(new_func, f)
+
+
+@cli.command("read")
+@processor
+def read_marc(reader):
     """
-    Read MARC records and print in terminal
+    Print MARC records in terminal
     """
-    records = read_marc_records(file)
     n = 0
     while True:
-        for record in records:
+        for record in reader:
             n += 1
             console.print(f"Printing record [marc_record]#{n}[/]")
             console.print(record)
+            yield record
             click.pause(info="Press any key to read next record")
         console.print("No more records")
         break
 
 
-@cli.command("read-records")
-@click.option(
-    "-f",
-    "--file",
-    prompt="File",
-    help="Path to file you would like to read or validate.",
-)
-def read_records(file):
+@cli.command("validate-record")
+@processor
+def validate(reader):
     """
-    Print converted MARC in terminal
-    """
-    records = read_marc_records(file)
-    n = 0
-    while True:
-        for record in records:
-            n += 1
-            converted_record = get_record_input(record)
-            console.print(f"Printing record [marc_record]#{n}[/]")
-            console.print(converted_record)
-            click.pause(info="Press any key to read next record")
-        console.print("No more records")
-        break
+    Validate records and print output one-by-one
 
-
-@cli.command("validate-records")
-@click.option(
-    "-f",
-    "--file",
-    prompt="File",
-    help="Path to file you would like to read or validate.",
-)
-def validate_records(file):
     """
-    Read and validate records one-by-one
-    """
-    reader = read_marc_records(file)
     n = 0
     while True:
         for record in reader:
@@ -98,9 +92,8 @@ def validate_records(file):
             if record_type == "monograph_record":
                 try:
                     MonographRecord(**record_input)
-                    console.print(
-                        f"\n\n[record_number]Record #{n}[/] (control_no [control_no]{control_number}[/]) is valid."
-                    )
+                    output = f"\n\n[record_number]Record #{n}[/] (control_no [control_no]{control_number}[/]) is valid."
+                    console.print(output)
                 except ValidationError as e:
                     formatted_errors = format_error_messages(e)
                     error_count = e.error_count()
@@ -109,9 +102,13 @@ def validate_records(file):
                     )
                     for error in formatted_errors:
                         if error["input"] == error["loc"]:
-                            console.print(f"\t{error['msg']}: {error['loc']}")
+                            output = f"\t{error['msg']}: {error['loc']}"
+                            console.print(output)
 
                         else:
+                            output = (
+                                f"\t{error['msg']}: {error['input']} {error['loc']}"
+                            )
                             console.print(
                                 f"\t{error['msg']}: {error['input']} {error['loc']}"
                             )
@@ -119,9 +116,8 @@ def validate_records(file):
             else:
                 try:
                     OtherMaterialRecord(**record_input)
-                    console.print(
-                        f"\n\n[record_number]Record #{n}[/] (control_no [control_no]{control_number}[/]) is valid."
-                    )
+                    output = f"\n\n[record_number]Record #{n}[/] (control_no [control_no]{control_number}[/]) is valid."
+                    console.print(output)
                 except ValidationError as e:
                     formatted_errors = format_error_messages(e)
                     error_count = e.error_count()
@@ -130,29 +126,47 @@ def validate_records(file):
                     )
                     for error in formatted_errors:
                         if error["input"] == error["loc"]:
-                            console.print(f"\t{error['msg']}: {error['loc']}")
+                            output = f"\t{error['msg']}: {error['loc']}"
+                            console.print(output)
 
                         else:
+                            output = (
+                                f"\t{error['msg']}: {error['input']} {error['loc']}"
+                            )
                             console.print(
                                 f"\t{error['msg']}: {error['input']} {error['loc']}"
                             )
-            click.pause(info="Press any key to read and validate next record\n")
+            yield output
+            click.pause(info="Press any key to read next record")
+        console.print("No more records")
+        break
+
+
+@cli.command("read-input")
+@processor
+def read_input(reader):
+    """
+    Convert MARC record to input, print to terminal
+    """
+    n = 0
+    while True:
+        for record in reader:
+            n += 1
+            converted_record = get_record_input(record)
+            console.print(f"Printing record [marc_record]#{n}[/]")
+            console.print(converted_record)
+            yield converted_record
+            click.pause(info="Press any key to read next record")
         console.print("No more records")
         break
 
 
 @cli.command("validate-all")
-@click.option(
-    "-f",
-    "--file",
-    prompt="File",
-    help="Path to file you would like to read or validate.",
-)
-def validate_all(file):
+@processor
+def validate_all(reader):
     """
-    Validate all records in a file
+    Validate all records in file, print all errors
     """
-    reader = read_marc_records(file)
     n = 0
     while True:
         console.print("\n\nChecking all records...")
@@ -165,9 +179,8 @@ def validate_all(file):
             if record_type == "monograph_record":
                 try:
                     MonographRecord(**record_input)
-                    console.print(
-                        f"\n\n[record_number]Record #{n}[/] (control_no [control_no]{control_number}[/]) is valid."
-                    )
+                    output = f"\n\n[record_number]Record #{n}[/] (control_no [control_no]{control_number}[/]) is valid."
+                    console.print(output)
                 except ValidationError as e:
                     formatted_errors = format_error_messages(e)
                     error_count = e.error_count()
@@ -176,19 +189,20 @@ def validate_all(file):
                     )
                     for error in formatted_errors:
                         if error["input"] == error["loc"]:
-                            console.print(f"\t{error['msg']}: {error['loc']}")
+                            output = f"\t{error['msg']}: {error['loc']}"
+                            console.print(output)
 
                         else:
-                            console.print(
+                            output = (
                                 f"\t{error['msg']}: {error['input']} {error['loc']}"
                             )
+                            console.print(output)
 
             else:
                 try:
                     OtherMaterialRecord(**record_input)
-                    console.print(
-                        f"\n\n[record_number]Record #{n}[/] (control_no [control_no]{control_number}[/]) is valid."
-                    )
+                    output = f"\n\n[record_number]Record #{n}[/] (control_no [control_no]{control_number}[/]) is valid."
+                    console.print(output)
                 except ValidationError as e:
                     formatted_errors = format_error_messages(e)
                     error_count = e.error_count()
@@ -196,17 +210,59 @@ def validate_all(file):
                         f"\n\nRecord [marc_record]#{n}[/] contains [error_count]{error_count} error(s)[/]"
                     )
                     for error in formatted_errors:
-                        if error["type"] == "union_tag_invalid":
-                            console.print(error)
-                            break
                         if error["input"] == error["loc"]:
-                            console.print(f"\t{error['msg']}: {error['loc']}")
+                            output = f"\t{error['msg']}: {error['loc']}"
+                            console.print(output)
 
                         else:
-                            console.print(
+                            output = (
                                 f"\t{error['msg']}: {error['input']} {error['loc']}"
                             )
-        console.print("Finished checking records.")
+                            console.print(output)
+        yield output
+        break
+
+
+@cli.command("validate-brief")
+@processor
+def validate_summary(reader):
+    """
+    Validate all records in file, print summary of errors
+    """
+    total_records = 0
+    invalid_records = 0
+    valid_records = 0
+    errored_records = []
+    while True:
+        for record in reader:
+            total_records += 1
+            control_number = record["001"].data
+            record_input = get_record_input(record)
+            record_type = get_material_type(record)
+            if record_type == "monograph_record":
+                try:
+                    MonographRecord(**record_input)
+                    valid_records += 1
+                except ValidationError as e:
+                    invalid_records += 1
+                    error_count = e.error_count()
+                    error_output = (control_number, error_count)
+                    errored_records.append(error_output)
+            else:
+                try:
+                    OtherMaterialRecord(**record_input)
+                    valid_records += 1
+                except ValidationError as e:
+                    invalid_records += 1
+                    error_count = e.error_count()
+                    error_output = (control_number, error_count)
+                    errored_records.append(error_output)
+        if invalid_records > 0:
+            output = f"\nFile contains {total_records} record(s): {valid_records} valid record(s) and [error_count]{invalid_records} invalid record(s)[/]. \n\n[error_count]Invalid record list and error count:[/] \n{errored_records} \n\nFor more detailed error information run `validator validate-all`"
+        else:
+            output = f"\nFile contains {total_records} valid record(s)"
+        yield output
+        console.print(output)
         break
 
 
