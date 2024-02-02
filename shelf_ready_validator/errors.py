86fb323e5546575e11c78a74f0e6fc7b011fcc49
Charlotte Kostelic <charlottekostelic@gmail.com>
2024-02-02 17:53:12 -0500

    editing doc strings and fixed typing

--- shelf_ready_validator/errors.py
+++ shelf_ready_validator/errors.py
@@ -1,270 +1,231 @@
-from typing import List, Dict
-
+from typing import Any
 from pydantic import ValidationError
+from pydantic_core import ErrorDetails
 from shelf_ready_validator.translate import RLMarcEncoding
 
 
-def missing_errors(error):
+def missing_errors(error: ErrorDetails) -> dict:
     """
-    A function to convert a standard pydantic error output
-    a function that handles errors for missing fields
-    converts error["loc"] values to MARC tags
-    flattens nested fields for item records to assign correct tag
+    A function to convert error location to MARC tags for "missing" error type.
+    Converts error["loc"] values to MARC tags and flattens nested fields
+    for item records. Item locations include item number and MARC tag for subfield.
+
+    Args:
+        error: pydantic ErrorDetails object
+
+    Returns:
+        error data as dict with edited error["loc"] value
 
     """
-    error["input"] = error["loc"]
+    new_error: dict = {}
+    new_error.update(error)
+    new_error["input"] = error["loc"]
     if error["loc"][0] == "items" and len(error["loc"]) == 4:
-        error["loc"] = (
+        new_error["loc"] = (
             f"item_{error['loc'][1]}",
-            RLMarcEncoding[error["loc"][3]].value,
+            RLMarcEncoding[str(error["loc"][3])].value,
         )
-        return error
+        return new_error
     else:
-        error["loc"] = RLMarcEncoding[error["loc"][0]].value
-        return error
+        new_error["loc"] = RLMarcEncoding[str(error["loc"][0])].value
+        return new_error
 
 
-def extra_errors(error):
+def extra_errors(error: ErrorDetails) -> dict:
     """
-    A function to convert a standard pydantic error output
-    a function that handles errors for extra fields
-    converts error["loc"] values to MARC tags
-    flattens nested fields in error["input"] for item records
+    A function to convert error location to MARC tags for "extra_forbidden" error type.
+    Converts error["loc"] values to MARC tags and adds suffix for multiple items.
+
+    Args:
+        error: pydantic ErrorDetails object
+
+    Returns:
+        error data as dict with edited error["loc"] value
 
     """
+    new_error: dict = {}
+    new_error.update(error)
     if type(error["input"]) is str:
-        error["loc"] = RLMarcEncoding[error["loc"][0]].value
-        return error
+        new_error["loc"] = RLMarcEncoding[str(error["loc"][0])].value
+        return new_error
     else:
-        error["loc"] = [
-            f"{RLMarcEncoding[error['loc'][0]].value}_" + str(i)
+        new_error["loc"] = [
+            f"{RLMarcEncoding[str(error['loc'][0])].value}_" + str(i)
             for i in range(len(error["input"]))
         ]
-        return error
+        return new_error
 
 
-def item_order_errors(error):
+def item_order_errors(error: ErrorDetails) -> dict:
     """
-    A function to convert a standard pydantic error output
-    a function that handles errors incorrect item/order combinations
-    converts error["loc"] values to MARC tags
-    includes which item is associated with incorrect data
-    returns MARC tags for item_location, item_type, and order_location
+    A function to convert error location to MARC tags for "Item/Order location check"
+    error type. Converts error["loc"] values to MARC tags and adds item number
+    associated with incorrect data. Returns MARC tags for item_location, item_type, and
+    order_location.
+    Args:
+        error: pydantic ErrorDetails object
+
+    Returns:
+        error data as dict with edited error["loc"] value
 
     """
-    error["loc"] = (
+    new_error: dict = {}
+    new_error.update(error)
+    new_error["loc"] = (
         f"item_{error['loc'][0]}",
-        (
-            RLMarcEncoding[error["loc"][1]].value,
-            RLMarcEncoding[error["loc"][2]].value,
-            RLMarcEncoding[error["loc"][3]].value,
-        ),
+        str(RLMarcEncoding[str(error["loc"][1])].value),
+        str(RLMarcEncoding[str(error["loc"][2])].value),
+        str(RLMarcEncoding[str(error["loc"][3])].value),
     )
-    return error
+    return new_error
 
 
-def match_errors(error):
+def match_errors(error: ErrorDetails) -> dict:
     """
-    A function to convert a standard pydantic error output
-    matches error type and pattern for string, literal, and union errors
-    converts error["loc"] to values with MARC tags
+    A function to convert error location to MARC tags and edit error message
+    for "literal_error", "string_pattern_mismatch" and "union_tag_invalid" error types.
+    Converts error["loc"] values to MARC tags and adds item number associated
+    with incorrect data.
 
-    Returns a new_error dict object containing:
-     - error type: str
-     - error location: tuple
-     - error input: dict
-     - error message: str
-     - error ctx: str
+    Args:
+        error: pydantic ErrorDetails object
 
+    Returns:
+        error data as dict with edited error["loc"] and error["msg"] values
 
     """
+    new_error: dict = {}
+    new_error.update(error)
     if error["loc"][0] == "items" and len(error["loc"]) == 4:
-        error["loc"] = (
+        new_error["loc"] = (
             f"item_{error['loc'][1]}",
-            RLMarcEncoding[error["loc"][3]].value,
+            RLMarcEncoding[str(error["loc"][3])].value,
         )
     elif error["loc"][0] == "items" and len(error["loc"]) == 2:
-        error["loc"] = (
+        new_error["loc"] = (
             f"item_{error['loc'][1]}",
-            RLMarcEncoding[error["loc"][0]].value,
+            RLMarcEncoding[str(error["loc"][0])].value,
         )
     else:
-        error["loc"] = RLMarcEncoding[error["loc"][0]].value
+        new_error["loc"] = RLMarcEncoding[str(error["loc"][0])].value
     match (error["type"], error["ctx"]):
         case (
             "literal_error",
-            {"expected": " "},
+            {"expected": "' '"},
         ):
-            error["msg"] = "Invalid indicator"
-            return error
+            new_error["msg"] = "Invalid indicator"
+            return new_error
         case (
             "literal_error",
-            {"expected": "1"},
+            {"expected": "'1'"},
         ):
-            error["msg"] = "Invalid indicator"
-            return error
+            new_error["msg"] = "Invalid indicator"
+            return new_error
         case (
             "literal_error",
             {"expected": "'EVP' or 'AUXAM'"},
         ):
-            error["msg"] = "Invalid vendor code"
-            return error
+            new_error["msg"] = "Invalid vendor code"
+            return new_error
         case (
             "literal_error",
             {"expected": "'RL'"},
         ):
-            error["msg"] = "Invalid library identifier"
-            return error
+            new_error["msg"] = "Invalid library identifier"
+            return new_error
         case (
             "literal_error",
             {"expected": "'8528'"},
         ):
-            error["msg"] = "Invalid item call tag"
-            return error
+            new_error["msg"] = "Invalid item call tag"
+            return new_error
         case (
             "literal_error",
             {
                 "expected": "'rcmb2', 'rcmf2', 'rcmg2', 'rc2ma', 'rcmp2', 'rcph2', 'rcpm2', 'rcpt2' or 'rc2cf'"
             },
         ):
-            error["msg"] = "Item location does not match a valid location"
-            return error
+            new_error["msg"] = "Item location does not match a valid location"
+            return new_error
         case (
             "literal_error",
             {
                 "expected": "'MAB', 'MAF', 'MAG', 'MAL', 'MAP', 'MAS', 'PAD', 'PAH', 'PAM', 'PAT' or 'SC'"
             },
         ):
-            error["msg"] = "Order location does not match a valid location"
-            return error
+            new_error["msg"] = "Order location does not match a valid location"
+            return new_error
         case ("literal_error", {"expected": "'43'"}):
-            error["msg"] = "Invalid item agency code"
-            return error
+            new_error["msg"] = "Invalid item agency code"
+            return new_error
         case (
             "string_pattern_mismatch",
             {"pattern": "^33433[0-9]{9}$|^33333[0-9]{9}$|^34444[0-9]{9}$"},
         ):
-            error["msg"] = "Invalid barcode"
-            return error
+            new_error["msg"] = "Invalid barcode"
+            return new_error
         case (
             "string_pattern_mismatch",
             {"pattern": "^ReCAP 23-\\d{6}$|^ReCAP 24-\\d{6}$"},
         ):
-            error["msg"] = "Invalid ReCAP call number"
-            return error
+            new_error["msg"] = "Invalid ReCAP call number"
+            return new_error
         case (
             "string_pattern_mismatch",
             {"pattern": "^\\d{3,}$"} | {"pattern": "^\\d{1,}$"},
         ):
-            error["msg"] = "Invalid price; price should not include a decimal point"
-            return error
+            new_error["msg"] = "Invalid price; price should not include a decimal point"
+            return new_error
         case (
             "string_pattern_mismatch",
             {"pattern": "^\\d{6}$"},
         ):
-            error["msg"] = "Invalid date; invoice date should be YYMMDD"
-            return error
+            new_error["msg"] = "Invalid date; invoice date should be YYMMDD"
+            return new_error
         case (
             "string_pattern_mismatch",
             {"pattern": "^\\d{1,}\\.\\d{2}$"},
         ):
-            error["msg"] = "Invalid price; item price should include a decimal point"
-            return error
+            new_error["msg"] = (
+                "Invalid price; item price should include a decimal point"
+            )
+            return new_error
         case (
             "string_pattern_mismatch",
             {"pattern": "^[^a-z]+"},
         ):
-            error["msg"] = "Invalid item message; message should be in all caps"
-            return error
+            new_error["msg"] = "Invalid item message; message should be in all caps"
+            return new_error
         case _:
             pass
-    return error
+    return new_error
 
 
-def format_errors(e: ValidationError) -> List:
+def format_errors(e: ValidationError) -> dict[str, Any]:
     """
-    A function to convert a list of errors based on error type
-    returns error["loc"] values with MARC tags
+    A function to format a list of errors based on error type.
+    Formats data to make it easier read when printed to terminal during validation.
+    Concatenates missing and extra fields into single item in list to make it easier to
+    read when printed to terminal during validation.
 
-    Returns a list of dicts containing
-     - error type: str
-     - error location: tuple
-     - error input: dict
-     - error message: str
-     - error ctx: str (optional)
+    Args:
+        e: ValidationError (output from pydantic model)
+
+    Returns:
+        dict:
+            error_count: int
+            missing_field_count: int
+            missing_fields: list
+            extra_field_count: int
+            extra_fields: list
+            invalid_field_count: int
+            invalid_fields: list
+            other_errors: list
+            other_error_fields: list
+            errors: list
 
     """
     errors = []
-    missing_fields = []
-    extra_fields = []
-    for error in e.errors():
-        if error["type"] == "missing":
-            converted_error = missing_errors(error)
-            missing_fields.append(converted_error["loc"])
-        elif error["type"] == "extra_forbidden":
-            converted_error = extra_errors(error)
-            if type(converted_error["loc"]) is list:
-                for loc in converted_error["loc"]:
-                    extra_fields.append(loc)
-            else:
-                extra_fields.append(converted_error["loc"])
-        elif error["type"] == "Item/Order location check":
-            converted_error = item_order_errors(error)
-            converted_error["count"] = 1
-            errors.append(converted_error)
-        else:
-            converted_error = match_errors(error)
-            converted_error["count"] = 1
-            errors.append(converted_error)
-    missing_fields[:] = (
-        value
-        for value in missing_fields
-        if value not in ("852_ind1", "852_ind2", "949_ind1", "949_ind2")
-    )
-    missing_field_count = len(missing_fields)
-    extra_fields[:] = (
-        value
-        for value in extra_fields
-        if value not in ("852_ind1", "852_ind2", "949_ind1", "949_ind2")
-    )
-    extra_field_count = len(extra_fields)
-    if len(missing_fields) > 0:
-        missing_field_error = {
-            "count": missing_field_count,
-            "type": "missing",
-            "loc": missing_fields,
-            "input": missing_fields,
-            "msg": "missing field/subfield(s)",
-        }
-        errors.append(missing_field_error)
-    if len(extra_fields) > 0:
-        extra_field_error = {
-            "count": extra_field_count,
-            "type": "extra_forbidden",
-            "loc": extra_fields,
-            "input": extra_fields,
-            "msg": "extra field/subfield(s)",
-        }
-        errors.append(extra_field_error)
-    return errors
-
-
-def format_error_summary(e: ValidationError) -> Dict:
-    """
-    A function to convert a list of errors based on error type
-    returns summary of errors based on type values with MARC tags
-
-    Returns a dicts containing
-     - missing_field_count:
-     - missing_fields:
-     - extra_field_count:
-     - extra_fields:
-     - invalid_field_count:
-     - invalid_fields:
-     - combination_error_count:
-     - combination_errors:
-
-    """
     missing_fields = []
     extra_fields = []
     invalid_fields = []
@@ -276,16 +237,26 @@ def format_error_summary(e: ValidationError) -> Dict:
             missing_fields.append(converted_error["loc"])
         elif error["type"] == "extra_forbidden":
             converted_error = extra_errors(error)
-            extra_fields.append(converted_error["loc"])
+            if type(converted_error["loc"]) is list:
+                for loc in converted_error["loc"]:
+                    extra_fields.append(loc)
+            else:
+                extra_fields.append(str(converted_error["loc"]))
         elif error["type"] == "Item/Order location check":
             converted_error = item_order_errors(error)
-            other_errors.append("Check item and order data; combination is not valid.")
+            other_errors.append(
+                f"Invalid item/order data combination in {converted_error['loc'][0]}"
+            )
             other_error_fields.append(converted_error["loc"])
+            errors.append(converted_error)
         elif error["type"] == "union_tag_invalid":
             other_errors.append("Unable to validate item record; invalid 910$a")
+            converted_error = match_errors(error)
+            errors.append(converted_error)
         else:
             converted_error = match_errors(error)
             invalid_fields.append(converted_error["loc"])
+            errors.append(converted_error)
     missing_fields[:] = (
         value
         for value in missing_fields
@@ -297,19 +268,15 @@ def format_error_summary(e: ValidationError) -> Dict:
         if value not in ("852_ind1", "852_ind2", "949_ind1", "949_ind2")
     )
     error_summary = {
-        "error_count": str(
-            len(missing_fields)
-            + len(extra_fields)
-            + len(invalid_fields)
-            + len(other_errors)
-        ),
-        "missing_field_count": str(len(missing_fields)),
+        "error_count": len(missing_fields) + len(extra_fields) + len(errors),
+        "missing_field_count": len(missing_fields),
         "missing_fields": missing_fields,
-        "extra_field_count": str(len(extra_fields)),
+        "extra_field_count": len(extra_fields),
         "extra_fields": extra_fields,
-        "invalid_field_count": str(len(invalid_fields)),
+        "invalid_field_count": len(invalid_fields),
         "invalid_fields": invalid_fields,
         "other_errors": other_errors,
         "other_error_fields": other_error_fields,
+        "errors": errors,
     }
     return error_summary
